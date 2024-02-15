989c87eae263b3c0a37259c7ea14c579daed4765
ritishbhardwaj <ritishbhardwaj02@gmail.com>
2024-02-15 16:03:57 +0530

    " Creating provider"

--- lib/todo_provider.dart
+++ lib/todo_provider.dart
@@ -1,8 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_application/todo.dart';
 
+
+
+
+
 class TodoProvider extends StateNotifier<List<Todo>>{
 
-  
-  
+  TodoProvider() : super([]);
+
+  void addTodo(Todo todo){
+    state=[...state,todo];    // this is spread operator  
+  }
+  void removeTodo(Todo todo){
+    state=state.where((element) => element.id!= todo.id).toList();
+  }
+
 }
